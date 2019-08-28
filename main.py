778079c8218da40bfa6fb589c5eefbf2df117ebf
Charlie White <cjw20@iu.edu>
2019-08-28 12:39:07 -0400

    Updated license, changed main.py and README.

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('My name is Charlie White.')
+print('Currently my favorite game to play right now is Overwatch, but I also play a lot of RPG games.')
+print('As of now, I do not have any concerns about this class.')
+print('I am excited to learn more about the game making process. And I am also looking forward to exploring the tools we will use in class.')
+print('My stackoverflow user number is 11990342.')
+print('My github url is https://github.com/cjw20')
